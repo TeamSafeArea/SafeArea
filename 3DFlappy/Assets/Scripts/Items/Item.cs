fd2a2e7fd126cd1c05b49533b1140eb107a19b0e
SaiL522 <youhoty@gmail.com>
2017-02-06 00:50:27 +0900

    アイテムが画面外に出たら削除する処理

---
+++ 3DFlappy/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// アイテム
+/// Yuuho Aritomi
+/// 2017/02/06
+/// </summary>
+public class Item : MonoBehaviour
+{
+    //画面左上
+    [SerializeField]
+    Vector2 m_min;
+    //画面右下
+    [SerializeField]
+    Vector2 m_max;
+    
+    //初期化
+    private void Start()
+    {
+
+    }
+    
+    //更新
+    private void Update()
+    {
+        Destroy();
+    }
+
+    //削除処理
+    private void Destroy()
+    {
+        Vector2 position = new Vector2(transform.position.x, transform.position.y);
+        if (m_min.x > position.x)
+        {
+            DestroyObject();
+            return;
+        }
+        if (m_min.y < position.y)
+        {
+            DestroyObject();
+            return;
+        }
+        if (m_max.x < position.x)
+        {
+            DestroyObject();
+            return;
+        }
+        if (m_max.y > position.y)
+        {
+            DestroyObject();
+            return;
+        }
+    }
+
+    //オブジェクトの削除
+    private void DestroyObject()
+    {
+        Destroy(transform.gameObject);
+    }
+}
