02259cb7019f36b18ec46eaa53d29721949a98df
SaiL522 <youhoty@gmail.com>
2017-02-08 09:40:56 +0900

    Edit : FIreBall.cs

---
+++ 3DFlappy/Assets/Character/Character/Player.cs
@@ -0,0 +1,80 @@
+﻿/// <summary>
+/// プレイヤークラス
+/// YuuhoAritomi
+/// 2017/02/05
+/// </summary>
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤークラス
+/// </summary>
+public class Player : MonoBehaviour
+{
+    //ジャンプする力
+    [SerializeField]
+    private float m_jumpPower;
+    //どこまで飛ぶか
+    [SerializeField]
+    private float m_maxJump;
+    //ジャンプしているか？
+    private bool m_isJump;
+
+    //初期化
+    public void Start()
+    {
+        m_isJump = false;
+    }
+
+    //更新
+    private void Update()
+    {
+        RestrictJump();
+    }
+
+    //更新
+    private void FixedUpdate()
+    {
+        Jump();
+    }
+
+    //他のコリジョンに当たっているときの処理
+    private void OnCollisionEnter(Collision collision)
+    {
+        IsGround(collision);
+    }
+
+    private void RestrictJump()
+    {
+        if (m_isJump == false) return;
+
+        Vector3 position = this.transform.localPosition;
+        position.y = Math.Min(position.y, m_maxJump);
+        this.transform.position = position;
+    }
+
+    //ジャンプ処理
+    private void Jump()
+    {
+
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+
+        if (m_isJump == false)
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+            rigidbody.AddForce(0f, m_jumpPower, 0f);
+            m_isJump = true;
+        }
+    }
+
+    // 着地しているか？
+    private void IsGround(Collision _col)
+    {
+        if (_col.transform.tag.Contains("Floor"))
+        {
+            m_isJump = false;
+        }
+    }
+}
---
+++ 3DFlappy/Assets/Item/Scripts/FireBall.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 m_power;
+    [SerializeField]
+    Rect m_clamp;
+
+    //初期化
+    private void Start()
+    {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(m_power);
+    }
+
+    //更新
+    private void Update()
+    {
+        Clamp();
+    }
+
+    private void Clamp()
+    {
+        Vector2 pos = transform.position;
+        ObjectDestroy(transform.gameObject, m_clamp.x > pos.x);
+        ObjectDestroy(transform.gameObject, m_clamp.y < pos.y);
+        ObjectDestroy(transform.gameObject, m_clamp.width < pos.x);
+        ObjectDestroy(transform.gameObject, m_clamp.height > pos.y);
+    }
+
+    private void ObjectDestroy(GameObject _obj, bool _flag, float _time = 0f)
+    {
+        if (!_flag) return;
+        Destroy(_obj, _time);
+    }
+}
